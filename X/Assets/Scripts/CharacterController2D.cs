ed119852f7de325db491b2a0c7db0b005bfab1fc
kishanmokaria <kishanmokaria@gmail.com>
2017-09-05 22:12:49 +0530

    Player Script

---
+++ X/Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement; // include so we can load new scenes
+
+public class CharacterController2D : MonoBehaviour {
+
+		public float moveSpeed = 3f;
+		// Use this for initialization
+		void Start () {
+
+		}
+
+		// Update is called once per frame
+		void Update ()
+		{
+			//Moves Forward and back along z axis                           //Up/Down
+			transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Vertical")* moveSpeed);
+			//Moves Left and right along x Axis                               //Left/Right
+			transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal")* moveSpeed);      
+		}
+	}
